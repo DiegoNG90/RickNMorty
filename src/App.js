9bed71b40b14614811a655d5eb8d243956d0b73e
Diego <diego.nicolas.gandara@gmail.com>
2021-12-30 22:41:37 -0300

    App main component will manage the state of the application, which originally comes from the https request to rickandmorty API. For that, it will use a CH called useFetch and then pass the findCharacter function to the Searchbar which is the main purpouse of the application.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
+import { useFetch } from './hooks/useFetch';
 import SearchBar from './components/SearchBar';
+import CharacterList from './components/Characters/CharacterList';
 import './App.css';
 
 function App() {
+  const [term, setTerm] = useState('');
+  const state = useFetch(term);
+  const { data, loading } = state;
+
+  const findCharacter = (e) => {
+    setTerm(`?name=${e.target.value}`);
+  };
   return (
     <>
-      <SearchBar />
+      <SearchBar findCharacter={findCharacter} />
+      <CharacterList data={data} loading={loading} />
     </>
   );
 }
