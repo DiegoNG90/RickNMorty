4c66265b613515bd8014567fd35bb5a63fcf27b2
Diego <diego.nicolas.gandara@gmail.com>
2022-01-02 02:51:53 -0300

    Create a Paginator component using the Pagination react-bootstrap UI component boilerplate and passing some props to make it work as expected.

---
+++ src/components/Paginator/Paginator.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Pagination } from 'react-bootstrap';
+
+const Paginator = ({ data, selectPage, activePage, term }) => {
+  const { info } = data || [];
+  console.log(`info`, info);
+
+  let items = [];
+  for (let number = 1; number <= info?.pages; number++) {
+    items.push(
+      <Pagination.Item
+        onClick={() => selectPage(number, term)}
+        key={number}
+        active={number === activePage}
+      >
+        {number}
+      </Pagination.Item>
+    );
+  }
+
+  return (
+    <div
+      className="d-flex  justify-content-center align-items-center"
+      style={{ marginTop: '2rem' }}
+    >
+      <Pagination className="d-flex flex-wrap" size="lg">
+        {items}
+      </Pagination>
+      <br />
+    </div>
+  );
+};
+
+export default Paginator;
