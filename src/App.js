9fb16a3fa7922678e77e4f6e385fd6729417971e
Diego <diego.nicolas.gandara@gmail.com>
2022-01-02 03:03:46 -0300

    Add the pagination component and pass the props. Also create the selectPage function which handles the different scenarios. And finally, findCharacters was a little bit refactored too.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import { useState } from 'react';
 import { useFetch } from './hooks/useFetch';
+import './App.css';
+
 import SearchBar from './components/SearchBar';
 import CharacterList from './components/Characters/CharacterList';
-import './App.css';
+import Paginator from './components/Paginator';
 
 function App() {
   const [term, setTerm] = useState('');
+  const [activePage, setActivePage] = useState(1);
   const state = useFetch(term);
   const { data, loading } = state;
 
   const findCharacter = (e) => {
-    setTerm(`?name=${e.target.value}`);
+    setActivePage(1);
+    setTerm(`?page=1&name=${e.target.value}`);
   };
+  const selectPage = (page, term = '') => {
+    setActivePage(page);
+    if (!term) {
+      setTerm(`?page=${page}`);
+    } else {
+      if (term.includes('page') && !term.includes('name')) {
+        setTerm(`?page=${page}`);
+      } else if (term.includes('name') && term.includes('page')) {
+        setTerm(`?page=${page}&?name=${term}`);
+      }
+    }
+  };
+
   return (
     <>
       <SearchBar findCharacter={findCharacter} />
       <CharacterList data={data} loading={loading} />
+      <Paginator
+        data={data}
+        selectPage={selectPage}
+        activePage={activePage}
+        term={term}
+      />
     </>
   );
 }
